c5e89469b87e1960c8e487085f0e8f8fe73d43d9
Aathreya D A <aathreyathebro@gmail.com>
2026-07-16 17:16:08 +0530

    Added basic security on PDF uploads

--- backend/app/services/pdf.py
+++ backend/app/services/pdf.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import shutil
 
+from fastapi import HTTPException
+
 UPLOAD_DIR = Path("uploads")
 
 
@@ -9,6 +11,11 @@ def save_pdf(file):
     Save an uploaded PDF into the uploads folder.
     """
 
+    if (file.content_type != "application/pdf" or not file.filename.lower().endswith(".pdf")):
+        raise HTTPException (
+            status_code=400,
+            detail="Only PDF files are allowed."
+        )
     UPLOAD_DIR.mkdir(exist_ok=True)
 
     destination = UPLOAD_DIR / file.filename
